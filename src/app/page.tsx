f28c29c1c199800774db07dfbd4347533be69867
Sudharsan-28705 <sudharsan.panimalarit@gmail.com>
2026-02-26 11:20:52 +0530

    bounce cards removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,25 +8,9 @@ import CountingNumberAnimation from "@/components/sections/company";
 import ProjectsSection from "@/components/sections/projects-section";
 import ScrollingLogoMarquee from "@/components/sections/scroll";
 import Foot from "@/components/sections/footer";
-import BounceCards from "@/components/sections/BounceCards";
 
 export default function Home() {
-    const images = [
-        "/SUJITH_REMIGIUS/SUJITH_REMIGIUS.jpg",
-        "/SUJITH_REMIGIUS/SUJITH_REMIGIUS.jpg",
-        "/SUJITH_REMIGIUS/SUJITH_REMIGIUS.jpg",
-        "/SUJITH_REMIGIUS/SUJITH_REMIGIUS.jpg",
-        "/SUJITH_REMIGIUS/SUJITH_REMIGIUS.jpg"
-    ];
-
-    const transformStyles = [
-        "rotate(5deg) translate(-150px)",
-        "rotate(0deg) translate(-70px)",
-        "rotate(-5deg)",
-        "rotate(5deg) translate(70px)",
-        "rotate(-5deg) translate(150px)"
-    ];
-
+    
     const [isScrolled, setIsScrolled] = useState(false);
 
     useEffect(() => {
@@ -50,16 +34,6 @@ export default function Home() {
                 <CountingNumberAnimation />
                 <ScrollingLogoMarquee />
                 <ProjectsSection />
-                {/* <BounceCards
-                    className="custom-bounceCards my-20 mx-auto flex flex-col items-center justify-center w-full max-w-4xl"
-                    images={images}
-                    containerWidth={500}
-                    containerHeight={250}
-                    animationDelay={1}
-                    animationStagger={0.2}
-                    easeType="elastic.out(1, 0.5)"
-                    transformStyles={transformStyles} 
-                    enableHover /> */}
                 <Foot />
             </main>
 
